d7508ca1b0d5cabe6bf9eba6619d1e99b404b1f3
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-04-11 21:49:06 +0800

    Added some form files,

--- bottlesonbottles/BottlesOnBottles/grails-app/controllers/admin/AdminAreaController.groovy
+++ bottlesonbottles/BottlesOnBottles/grails-app/controllers/admin/AdminAreaController.groovy
@@ -8,4 +8,18 @@ class AdminAreaController {
     def index() {
         render 'Secure access only'
     }
+    
+    def editProducts() {
+        render 'Strech Goal'
+    }
+    
+    def newProduct() {
+        //Add to Product db 
+        //For now will have == codes overwrite so can edit aswell
+    }
+    
+    def newCatagory() {
+        //Add to Catagory db
+        
+    }
 }
