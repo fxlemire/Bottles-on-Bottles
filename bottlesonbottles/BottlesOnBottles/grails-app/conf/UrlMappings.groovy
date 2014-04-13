02e21a86be654c29c368b95cc982922f64cb9f1e
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-04-14 07:02:11 +0800

    Getting Homepage Index

--- bottlesonbottles/BottlesOnBottles/grails-app/conf/UrlMappings.groovy
+++ bottlesonbottles/BottlesOnBottles/grails-app/conf/UrlMappings.groovy
@@ -11,7 +11,7 @@ class UrlMappings {
         "500"(view:'/error')
         
         "/" {
-            controller = "HomePageController"
+            controller = "HomePage"
             action = "index"
         }
 	}
--- bottlesonbottles/BottlesOnBottles/grails-app/controllers/bottlesonbottles/HomePageController.groovy
+++ bottlesonbottles/BottlesOnBottles/grails-app/controllers/bottlesonbottles/HomePageController.groovy
@@ -1,11 +1,14 @@
 package bottlesonbottles
 
+
 import grails.plugin.springsecurity.annotation.Secured
 class HomePageController {
 
     @Secured(['ROLE_ADMIN'])
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond Product.list(params), model:[productInstanceCount: Product.count()]
+        List<Product> productInstanceList = Product.list(params)
+        render (view : '/index', model : [productInstanceList : productInstanceList])
+       
     }
 }
