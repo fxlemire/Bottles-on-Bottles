d0815486d79bc75967a150c9f9cdbb31b6d4913f
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-04-26 20:13:38 +0800

    more searching still strict

--- bottlesonbottles/BottlesOnBottles/grails-app/controllers/bottlesonbottles/HomePageController.groovy
+++ bottlesonbottles/BottlesOnBottles/grails-app/controllers/bottlesonbottles/HomePageController.groovy
@@ -17,7 +17,8 @@ class HomePageController {
 	
     @Secured(['ROLE_USER','ROLE_ADMIN'])
     def searchProducts() {
-        List<Product> productInstanceList = Product.search(params.name, [result:'every']/*params.name /*upc: upc:params.upc, price:params.price, aoc:params.aoc, country:params.country, size:params.size, percent:params.int('percent'), age:params.int('age')*/)
+        //List<Product> productInstanceList = Product.search(params.name, [result:'every']/*params.name /*upc: upc:params.upc, price:params.price, aoc:params.aoc, country:params.country, size:params.size, percent:params.int('percent'), age:params.int('age')*/)
+        def productInstanceList = Product.findAllWhere([name:params.name, /*upc:params.upc,*/ price:params.price, aoc:params.aoc, country:params.country, size:params.size, percent:params.int('percent'), age:params.int('age')])
 	render (view : '/index', model : [productInstanceList: productInstanceList])
     }
 }
